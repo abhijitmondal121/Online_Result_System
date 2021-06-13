b48a1182c7332f86d3c71a675a08d1c2ccc35328
ABHIJIT MONDAL <70851067+abhijitmondal121@users.noreply.github.com>
2021-06-13 10:27:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
         p{
             font-weight:bold;
  			 text-shadow: 2px 2px white;
- 			 font-size:30px;
+ 			 font-size:25px;
  			 text-align:center;
  			 
         }
@@ -61,6 +61,183 @@
          .block:hover {
                 background-color:white;
                 color: black;
+                }   
+
+            @media screen and (min-device-width: 694px) and (max-device-width: 999px){
+
+                body
+                {
+                    background-image: linear-gradient(white,#ccff33,#00e600);
+                    height: 925px;
+                   
+                    
+                }
+                h1
+                {
+                    text-align: center;
+                    color:blue;
+                    text-shadow: 2px 2px white;
+                    font-size: 55px;
+                }
+                form
+                {   margin-top:2%;
+                    background-image: linear-gradient(white,#ff6600);
+                    padding: 10px 10px;
+                    width: 350px;
+                    margin-left:30%;
+                    border:5px solid  black;
+                    border-radius: 15px;
+                }
+                select
+                {
+                    width: 150px;
+                }
+                p{
+                    font-weight:bold;
+                      text-shadow: 2px 2px white;
+                      font-size:21px;
+                      text-align:center;
+                      
+                }
+
+                .block {
+                        display: block;
+                        width: 20%;
+                        border: none;
+                        background-color: black;
+                        color: white;
+                        padding:8px 2px;
+                        font-size: 16px;
+                        cursor: pointer;
+                        text-align: center;
+                        margin-left:38%;
+                        border-radius: 15px;
+                        }
+
+                 .block:hover {
+                        background-color:white;
+                        color: black;
+                        }
+
+                }
+
+
+
+   
+     @media screen and (min-device-width: 450px) and (max-device-width: 693px){
+
+                body
+                {
+                    background-image: linear-gradient(white,#ccff33,#00e600);
+            height: 925px;
+                   
+                    
+                }
+                h1
+                {
+                    text-align: center;
+                    color:blue;
+                    text-shadow: 2px 2px white;
+                    font-size: 50px;
+                }
+                form
+                {   margin-top:2%;
+                    background-image: linear-gradient(white,#ff6600);
+                    padding: 10px 10px;
+                    width: 300px;
+                    margin-left:22%;
+                    border:5px solid  black;
+                    border-radius: 15px;
+                }
+                select
+                {
+                    width: 150px;
+                }
+                p{
+                    font-weight:bold;
+                      text-shadow: 2px 2px white;
+                      font-size:17px;
+                      text-align:center;
+                      
+                }
+
+                .block {
+                        display: block;
+                        width: 20%;
+                        border: none;
+                        background-color: black;
+                        color: white;
+                        padding:8px 2px;
+                        font-size: 16px;
+                        cursor: pointer;
+                        text-align: center;
+                        margin-left:38%;
+                        border-radius: 15px;
+                        }
+
+                 .block:hover {
+                        background-color:white;
+                        color: black;
+                        }
+
+                }
+
+            @media screen and (min-device-width: 280px) and (max-device-width: 449px){
+
+                body
+                {
+                    background-image: linear-gradient(white,#ccff33,#00e600);
+                    height: 725px;
+                   
+                    
+                }
+                h1
+                {
+                    text-align: center;
+                    color:blue;
+                    text-shadow: 2px 2px white;
+                    font-size: 35px;
+                }
+                form
+                {   margin-top:2%;
+                    background-image: linear-gradient(white,#ff6600);
+                    padding: 10px 10px;
+                    width: 220px;
+                    margin-left:15%;
+                    border:5px solid  black;
+                    border-radius: 15px;
+                }
+                select
+                {
+                    width: 150px;
+                }
+                p{
+                    font-weight:bold;
+                      text-shadow: 2px 2px white;
+                      font-size:24px;
+                      text-align:center;
+                      
+                }
+
+                .block {
+                        display: block;
+                        width: 20%;
+                        border: none;
+                        background-color: black;
+                        color: white;
+                        padding:8px 2px;
+                        font-size: 12px;
+                        cursor: pointer;
+                        text-align: center;
+                        margin-left:38%;
+                        border-radius: 15px;
+                        }
+
+                 .block:hover {
+                        background-color:white;
+                        color: black;
+                        }
+
                 }
     </style>
 </head>
